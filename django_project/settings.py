dcfedab17f1c76ce873f60d6ed09fbc8171ccd83
ehsan <ehsanheidarzadeh959@gmail.com>
2022-07-29 22:48:17 +0430

    creating new app accounts

--- django_project/settings.py
+++ django_project/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+"default": {
+"ENGINE": "django.db.backends.postgresql",
+"NAME": "postgres",
+"USER": "postgres",
+"PASSWORD": "postgres",
+"HOST": "db",
+"PORT": 5432,
 }
+}
+
 
 
 # Password validation
